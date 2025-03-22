062103383f561c53f090b4133ac0ed81d80abd7a
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2025-03-22 18:27:08 +0200

    fix scripts

--- ETL_Processor/SqlScripts/create_indexes.sql
+++ ETL_Processor/SqlScripts/create_indexes.sql
@@ -1,8 +1,16 @@
-USE SampleDB;
-GO
 
-CREATE INDEX IX_PULocationID_TipAmount ON ProcessedData (PULocationID, tip_amount);
-CREATE INDEX IX_TripDistance ON ProcessedData (trip_distance DESC);
-CREATE INDEX IX_TripDuration ON ProcessedData (DATEDIFF(MINUTE, tpep_pickup_datetime, tpep_dropoff_datetime) DESC);
-CREATE INDEX IX_PULocationID ON ProcessedData (PULocationID);
-GO
+CREATE INDEX IX_PULocationID_AvgTipAmount
+ON ProcessedData (PULocationID)
+INCLUDE (tip_amount);
+
+CREATE INDEX IX_TripDistance
+ON ProcessedData (trip_distance DESC);
+
+CREATE INDEX IX_PULocationID
+ON ProcessedData (PULocationID);
+
+CREATE INDEX IX_StoreAndFwdFlag
+ON ProcessedData (store_and_fwd_flag);
+
+CREATE INDEX IX_PickupDropoffDateTime
+ON ProcessedData (tpep_pickup_datetime, tpep_dropoff_datetime);
--- ETL_Processor/SqlScripts/create_table.sql
+++ ETL_Processor/SqlScripts/create_table.sql
@@ -1,16 +1,12 @@
-USE SampleDB;
-GO
-
 CREATE TABLE ProcessedData (
-    tpep_pickup_datetime DATETIME NOT NULL,
-    tpep_dropoff_datetime DATETIME NOT NULL,
+    tpep_pickup_datetime DATETIME,
+    tpep_dropoff_datetime DATETIME,
     passenger_count INT NULL,
-    trip_distance FLOAT NULL,
-    store_and_fwd_flag VARCHAR(3) NOT NULL,
-    PULocationID INT NOT NULL,
-    DOLocationID INT NOT NULL,
-    fare_amount DECIMAL(10, 2) NULL,
-    tip_amount DECIMAL(10, 2) NULL,
-    PRIMARY KEY (tpep_pickup_datetime, tpep_dropoff_datetime, passenger_count)
+    trip_distance FLOAT,
+    store_and_fwd_flag VARCHAR(3),
+    PULocationID INT,
+    DOLocationID INT,
+    fare_amount DECIMAL(10, 2),
+    tip_amount DECIMAL(10, 2)
 );
 GO
