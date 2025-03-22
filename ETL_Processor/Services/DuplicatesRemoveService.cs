4d12aeacc2c0673d929a8a69e1f220c92ac8255a
Taras Bandrivskyi <113480166+Rivskyyy@users.noreply.github.com>
2025-03-22 15:53:11 +0200

    add DuplicatedRemoveService

--- ETL_Processor/Services/DuplicatesRemoveService.cs
+++ ETL_Processor/Services/DuplicatesRemoveService.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using ETL_Processor.Model;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +12,45 @@ namespace ETL_Processor.Services
 {
     public class DuplicatesRemoveService
     {
+        public async Task<IEnumerable<Record>> RemoveDuplicatesAsync(IAsyncEnumerable<Record> records)
+        {
+            var recordList = new List<Record>();
+            await foreach (var record in records)
+            {
+                recordList.Add(record);
+            }
 
+            return recordList.GroupBy(r => new { r.PickupDateTime, r.DropoffDateTime, r.PassengerCount })
+                             .Select(g => g.First());
+        }
+        public async Task<IEnumerable<Record>> GetDuplicatesAsync(IAsyncEnumerable<Record> transformedRecords, IEnumerable<Record> recordsWithoutDuplicates)
+        {
+            var transformedList = new List<Record>();
+            var duplicatesList = new List<Record>();
+
+   
+            await foreach (var record in transformedRecords)
+            {
+                transformedList.Add(record);
+            }
+
+            foreach (var record in recordsWithoutDuplicates)
+            {
+                if (transformedList.Contains(record))
+                {
+                    duplicatesList.Add(record);
+                }
+            }
+
+            return duplicatesList;
+        }
+        public async Task WriteDuplicatesToCsvAsync(IEnumerable<Record> duplicates, string filePath)
+        {
+            using (var writer = new StreamWriter(filePath))
+            using (var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)))
+            {
+                await csv.WriteRecordsAsync(duplicates);
+            }
+        }
     }
 }
